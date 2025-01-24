fe060240471c2ea32036e66ca2a432450233b164
Mahesh3000 <mahesh.sivngi@gmail.com>
2025-01-23 19:09:27 -0600

    handlcors error

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ const app = express();
 app.use(express.json());
 
 // app.use(cors());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     methods: "GET,POST,PUT,DELETE",
-//     credentials: true,
-//   })
-// );
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(bodyParser.json());
 
