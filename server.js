f16b40c720fe4b64b878dabccf9549dfb71c05ad
Mahesh3000 <mahesh.sivngi@gmail.com>
2025-01-23 19:45:10 -0600

    Allow Private IPs

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello, i am running fine");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
