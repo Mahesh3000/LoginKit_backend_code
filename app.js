e1f2c23606b91f3d3480f54b0c91c2f05c359aa6
Mahesh3000 <mahesh.sivngi@gmail.com>
2025-02-06 23:24:37 -0600

    deploy chagesg -13

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://18.234.47.219:5173",
+    origin: "http://maheshsivangi.tech:5173",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
