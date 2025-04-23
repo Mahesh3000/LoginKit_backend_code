e5237deec8d616f2bf09b031651f0bd00e9650f6
Mahesh3000 <mahesh.sivngi@gmail.com>
2025-04-23 16:43:18 -0500

    depl changes-4

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 const allowedOrigins = [
   "https://maheshsivangi.tech",
-  "https://cryptotrack.maheshsivangi.tech",
+  "https://loginkit.maheshsivangi.tech",
   "http://localhost:5173",
 ];
 
